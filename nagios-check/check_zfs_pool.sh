b3edce1a9e4c8c2fd5c1643251e11282005d9cea
Mikko Pervilä <pervila@remedy.fi>
2015-11-13 16:32:20 +0200

    warning & crit calculations should now work

--- nagios-check/check_zfs_pool.sh
+++ nagios-check/check_zfs_pool.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 e_ok=0
+e_ok_str="OK"
 e_warning=1
+e_warning_str="WARNING"
 e_critical=2
+e_critical_str="CRITICAL"
 e_unknown=3
+e_unknown_str="UNKNOWN"
 magic_mib="NET-SNMP-EXTEND-MIB"
 magic_oid="${magic_mib}::nsExtendOutLine"
 
@@ -38,7 +42,7 @@ while getopts "H:C:hvw:c:" Option; do
   esac
 done
 
-for value in $hostname $community $warning $critical; do
+for value in "$hostname" "$community" "$warning" "$critical"; do
  if [ -z "${value}" ]; then
  
    echo "ERROR: not enough parameters given."
@@ -55,13 +59,27 @@ if [ ! $? -eq 0 ]; then
   exit $e_unknown
 fi
 
-if [ "$verbose" = 1 ]; then cat $t ; fi
+if [ "$verbose" = 1 ]; then
+  echo "INFO: snmpwalk successful, results:"
+  cat $t
+fi
 
 zpool_name="$(grep zpool_name tests/sample_output.txt | sed -e 's/.*STRING: \(.\+\)\+/\1/')"
-zpool_capacity="$(grep zpool_capacity tests/sample_output.txt | sed -e 's/.*STRING: \([0-9]\+\)\+%/\1%/')"
+zpool_capacity="$(grep zpool_capacity tests/sample_output.txt | sed -e 's/.*STRING: \([0-9]\+\)\+%/\1/')"
 zpool_dedupratio="$(grep zpool_dedupratio tests/sample_output.txt | sed -e 's/.*STRING: \([0-9.]\+\)\+x/\1x/')"
 zpool_available="$(grep zpool_available tests/sample_output.txt | sed -e 's/.*STRING: \([0-9]\+\)\+/\1/')"
 zpool_used="$(grep zpool_used tests/sample_output.txt | sed -e 's/.*STRING: \([0-9]\+\)\+/\1/')"
 
-echo "$zpool_name status; usage $zpool_capacity; dedup ratio $zpool_dedupratio; ($zpool_used/$zpool_available)"
+err_code=$e_ok
+status=$e_ok_str
+if [ $zpool_capacity -ge "$warning" ]; then
+  err_code=$e_warning
+  status=$e_warning_str
+fi
+if [ $zpool_capacity -ge "$critical" ]; then
+  err_code=$e_critical
+  status=$e_critical_str 
+fi
+
+echo "zpool $zpool_name ${status}; usage ${zpool_capacity}%; dedup ratio $zpool_dedupratio; ($zpool_available/$zpool_used)"
 rm $t
