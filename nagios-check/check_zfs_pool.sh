1c6c01614af683383531b4cfc72579be502ca89e
Mikko Administrator <pervila-adm@monitor.remedy.fi>
2015-11-13 16:14:57 +0200

    This one correctly parses the strings, still needs output formatting

--- nagios-check/check_zfs_pool.sh
+++ nagios-check/check_zfs_pool.sh
@@ -3,12 +3,14 @@ e_ok=0
 e_warning=1
 e_critical=2
 e_unknown=3
-
-magic_oid="NET-SNMP-EXTEND-MIB NET-SNMP-EXTEND-MIB::nsExtendOutLine"
+magic_mib="NET-SNMP-EXTEND-MIB"
+magic_oid="${magic_mib}::nsExtendOutLine"
 
 usage() {
   echo "
-$0 -H hostname -C community"
+$0 -H hostname -C community
+
+Uses snmpwalk to fetch ${magic_oid} from hostname using community"
 }
 
 while getopts "H:C:hd" Option; do
@@ -17,7 +19,7 @@ while getopts "H:C:hd" Option; do
       hostname="${OPTARG}"
     ;;
     C)
-      snmpcommunity="${OPTARG}"
+      community="${OPTARG}"
     ;;
     *) 
       echo "ERROR: unimplemented parameter ${Option}"
@@ -27,10 +29,29 @@ while getopts "H:C:hd" Option; do
   esac
 done
 
-if [ -z "${hostname}" ] || [ -z "${snmpcommunity}" ]; then
+if [ -z "${hostname}" ] || [ -z "${community}" ]; then
   echo "ERROR: not enough parameters given."
   usage
   exit $e_unknown
 fi
 
+t=$(mktemp)
+snmpwalk -v 2c -c "${community}" -m "${magic_mib}"  "${hostname}" "${magic_oid}" > "$t"
+if [ ! $? -eq 0 ]; then
+  echo "ERROR: could not fetch snmp data from ${hostname}"
+  rm $t
+  exit $e_unknown
+fi
+cat $t
 
+zpool_capacity="$(grep zpool_capacity tests/sample_output.txt | sed -e 's/.*STRING: \([0-9]\+\)\+%/\1%/')"
+echo "cap is $zpool_capacity"
+zpool_dedupratio="$(grep zpool_dedupratio tests/sample_output.txt | sed -e 's/.*STRING: \([0-9.]\+\)\+x/\1x/')"
+echo "dedupratio is $zpool_dedupratio"
+# these two are almost the same, but I'm avoiding metaprogramming with Bash
+zpool_available="$(grep zpool_available tests/sample_output.txt | sed -e 's/.*STRING: \([0-9]\+\)\+/\1/')"
+echo "available is $zpool_available"
+zpool_used="$(grep zpool_used tests/sample_output.txt | sed -e 's/.*STRING: \([0-9]\+\)\+/\1/')"
+echo "used is $zpool_used"
+
+rm $t
